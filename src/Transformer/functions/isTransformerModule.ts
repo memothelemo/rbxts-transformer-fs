944b11e97340803f3b46989d65213e0de2bcbbcc
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-07-25 03:10:04 +0800

    Deleting all of the transformer files

--- src/Transformer/functions/isTransformerModule.ts
+++
@@ -1,6 +0,0 @@
-import ts from "byots";
-import { SOURCE_MODULE_TEXT } from "../../Shared/constants";
-
-export function isTransformerModule(sourceFile: ts.SourceFile) {
-	return sourceFile.text === SOURCE_MODULE_TEXT;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import ts from "byots";
-import { TransformState } from "./Transformer/state";
 
-export default function (program: ts.Program) {
-	return (
-		context: ts.TransformationContext,
-	): ((file: ts.SourceFile) => ts.SourceFile) => {
-		const state = new TransformState(program, context);
-		let transformed: Map<ts.SourceFile, ts.SourceFile>;
+/*
+	Refactoring:
+	export default function (program: ts.Program) {
+		return (
+			context: ts.TransformationContext,
+		): ((file: ts.SourceFile) => ts.SourceFile) => {
+			const state = new TransformState(program, context);
+			let transformed: Map<ts.SourceFile, ts.SourceFile>;
 
-		return file => {
-			if (!transformed) {
-				transformed = state.transformAll();
-			}
-			return transformed.get(file) ?? file;
+			return file => {
+				if (!transformed) {
+					transformed = state.transformAll();
+				}
+				return transformed.get(file) ?? file;
+			};
 		};
-	};
-}
+	}
+
+*/
