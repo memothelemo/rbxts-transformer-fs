afe69d220bd6efc1d2881c2028824f882e441e8a
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-07-30 19:40:38 +0800

    1.1.1 release

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,13 @@
+/**
+ * It replaces from filesystem path to Roblox structured tree
+ * @param path Filesystem path
+ */
 export function $path<T>(path: string): T;
+
+/**
+ * It replaces from filesystem path to Roblox structured tree
+ * with WaitForChild implemented
+ * @param path Filesystem path
+ * @param timeout
+ */
 export function $pathWaitFor<T>(path: string, timeout?: number): T;
