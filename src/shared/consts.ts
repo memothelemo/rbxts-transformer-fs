9bc98123459ea72b724d25c6202c1451128bc1fc
memothelemo <dev@memothelemo.xyz>
2024-10-22 19:12:39 +0800

    Dump everything

--- src/shared/consts.ts
+++
@@ -1,6 +0,0 @@
-import path from "path";
-
-export const PACKAGE_NAME = "rbxts-transformer-fs";
-export const PACKAGE_ISSUES_URL = "https://github.com/memothelemo/rbxts-transformer-fs/issues";
-
-export const MODULE_FILE_PATH = path.join(__dirname, "..", "..", "index.d.ts");
--- src/shared/util/overrideAsArray.ts
+++
@@ -1,5 +0,0 @@
-export function overrideAsArray<T>(
-  value: NonNullable<T> | NonNullable<T>[] | undefined,
-): NonNullable<T>[] {
-  return Array.isArray(value) ? value : value == undefined ? [] : [value];
-}
